58aa6514a27e9e787c0ad5d754b5f5f6b0862cd4
Ivo Anjo <ivo@ivoanjo.me>
2025-06-13 23:00:53 +0100

    Simplified definitions imported from Ruby

--- ext/direct_bind_native_extension/direct_bind.c
+++ ext/direct_bind_native_extension/direct_bind.c
@@ -36,47 +36,15 @@ typedef struct rb_method_entry_struct {
   VALUE owner;
 } rb_method_entry_t;
 
-// ### TODO: Cleanup a bit?
-#define VM_METHOD_TYPE_MINIMUM_BITS 4
-
-typedef VALUE (*rb_cfunc_t)(ANYARGS);
-typedef struct rb_method_cfunc_struct {
-  rb_cfunc_t func;
-  VALUE (*invoker)(VALUE recv, int argc, const VALUE *argv, VALUE (*func)(ANYARGS));
-  int argc;
-} rb_method_cfunc_t;
-
-#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
-# define BITFIELD(type, name, size) type name : size
-#else
-# define BITFIELD(type, name, size) unsigned int name : size
-#endif
-
-typedef enum {
-  VM_METHOD_TYPE_ISEQ,      /*!< Ruby method */
-  VM_METHOD_TYPE_CFUNC,     /*!< C method */
-  VM_METHOD_TYPE_ATTRSET,   /*!< attr_writer or attr_accessor */
-  VM_METHOD_TYPE_IVAR,      /*!< attr_reader or attr_accessor */
-  VM_METHOD_TYPE_BMETHOD,
-  VM_METHOD_TYPE_ZSUPER,
-  VM_METHOD_TYPE_ALIAS,
-  VM_METHOD_TYPE_UNDEF,
-  VM_METHOD_TYPE_NOTIMPLEMENTED,
-  VM_METHOD_TYPE_OPTIMIZED, /*!< Kernel#send, Proc#call, etc */
-  VM_METHOD_TYPE_MISSING,   /*!< wrapper for method_missing(id) */
-  VM_METHOD_TYPE_REFINED,   /*!< refinement */
-} rb_method_type_t;
-
+// ### This was simplified/inlined vs the original structure
 struct rb_method_definition_struct {
-  BITFIELD(rb_method_type_t, type, VM_METHOD_TYPE_MINIMUM_BITS);
-  unsigned int iseq_overload: 1;
-  unsigned int no_redef_warning: 1;
-  unsigned int aliased : 1;
-  int reference_count : 28;
-
-  union {
-    rb_method_cfunc_t cfunc;
-  } body;
+  unsigned int type: 4;
+  int _ignored;
+  struct {
+    VALUE (*func)(ANYARGS);
+    void *_ignored;
+    int argc;
+  } cfunc;
 };
 
 // # This is where the magic happens: Using objectspace to find the method entry and retrieve the cfunc
@@ -104,10 +72,11 @@ direct_bind_cfunc_result direct_bind_get_cfunc(VALUE klass, ID method_name, bool
   return find_data.result;
 }
 
-// TODO: Change this to use safe memory reads that can never segv (e.g. if structure layouts are off?)
+// TODO: Maybe change this to use safe memory reads that can never segv (e.g. if structure layouts are off?)
 static int find_cfunc(void *start, void *end, size_t stride, void *data) {
   const int stop_iteration = 1;
   const int continue_iteration = 0;
+  const int vm_method_type_cfunc = 1;
 
   find_data_t *find_data = (find_data_t *) data;
 
@@ -119,14 +88,14 @@ static int find_cfunc(void *start, void *end, size_t stride, void *data) {
 
     if (method_entry->def == NULL) {
       find_data->result.failure_reason = rb_str_new_lit("method_entry->def is NULL");
-    } else if (method_entry->def->type != VM_METHOD_TYPE_CFUNC) {
+    } else if (method_entry->def->type != vm_method_type_cfunc) {
       find_data->result.failure_reason = rb_str_new_lit("method_entry is not a cfunc");
     } else {
       find_data->result = (direct_bind_cfunc_result) {
         .ok = true,
         .failure_reason = Qnil,
-        .arity = method_entry->def->body.cfunc.argc,
-        .func = method_entry->def->body.cfunc.func,
+        .arity = method_entry->def->cfunc.argc,
+        .func = method_entry->def->cfunc.func,
       };
     }
     return stop_iteration;
